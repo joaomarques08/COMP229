da31afc390a884e1d255a7e70ed339f5b09839e6
Joao Marques <joaovitormarquesribeiro@outlook.com>
2023-09-11 12:06:16 -0400

    first commit

---
+++ const.js
@@ -0,0 +1,8 @@
+function reinitConst() {
+    const me = 1;
+    console.log(me);
+    me = 2;
+    console.log(me);
+}
+
+reinitConst();
---
+++ let.js
@@ -0,0 +1,7 @@
+function iterateVar() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+    console.log(i);
+}
+iterateVar();
